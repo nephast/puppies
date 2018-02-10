a385fcc3329a84a99449b0bca2ff65c44b7670bc
Stéphan Gaud <stephgoeseast@gmail.com>
2018-02-10 17:37:51 +0000

    all routes working

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ var db = require('../queries');
 
 router.get('/api/puppies', db.getAllPuppies);
 router.get('/api/puppies/:id', db.getSinglePuppy);
-// router.post('/api/puppies', db.createPuppy);
-// router.put('/api/puppies/:id', db.updatePuppy);
-// router.delete('/api/puppies/:id', db.removePuppy);
+router.post('/api/puppies', db.createPuppy);
+router.put('/api/puppies/:id', db.updatePuppy);
+router.delete('/api/puppies/:id', db.removePuppy);
 
 
 module.exports = router;
