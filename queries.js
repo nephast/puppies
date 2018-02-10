964f76d9622650912ca2557680ff2258b768e5e0
Stéphan Gaud <stephgoeseast@gmail.com>
2018-02-10 17:06:05 +0000

    first commit

---
+++ queries.js
@@ -0,0 +1,50 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://localhost:5432/puppies';
+var db = pgp(connectionString);
+
+// add query functions
+function getAllPuppies(req, res, next) {
+  db.any('select * from pups')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL puppies'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function getSinglePuppy(req, res, next) {
+  var pupID = parseInt(req.params.id);
+  db.one('select * from pups where id = $1', pupID)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ONE puppy'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+module.exports = {
+  getAllPuppies: getAllPuppies,
+  getSinglePuppy: getSinglePuppy,
+  // createPuppy: createPuppy,
+  // updatePuppy: updatePuppy,
+  // removePuppy: removePuppy
+};
